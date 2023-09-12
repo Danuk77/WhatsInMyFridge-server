91aba14f09723c84e56595384c7993603628f8e0
Danuk <danuk77@live.co.uk>
2023-09-12 23:17:20 +0100

    Added unique ID fields to all food items

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from pydantic import BaseModel
+from bson import ObjectId
 
 app = FastAPI(docs_url="/docs")
 
@@ -37,17 +38,36 @@ class FoodItem(BaseModel):
     quantity: int
     expirationType: str
 
+# Recursively convert objectID's to string
+
+
+def convert_objectid_to_str(data):
+    if isinstance(data, ObjectId):
+        return str(data)
+    elif isinstance(data, list):
+        return [convert_objectid_to_str(item) for item in data]
+    elif isinstance(data, dict):
+        return {key: convert_objectid_to_str(value) for key, value in data.items()}
+    else:
+        return data
+
 
 # Get the users information
 @app.get("/userInfo/{userName}")
 async def getUserInfo(userName: str):
-    data = col.find_one({"User": userName}, {'_id': 0})
+    data = col.find_one({"User": userName})
+    data = convert_objectid_to_str(data)
+
     return {"body": data}
 
 
+# Add new fooditem (generates unique ID)
 @app.post("/userItems/{userName}/{storageLocation}")
 async def postNewItem(userName: str, storageLocation: str, foodItem: FoodItem):
 
+    newItem = dict(foodItem)
+    newItem['_id'] = ObjectId()
+
     assert storageLocation in ("Fridge", "Freezer", "Shelf")
     col.update_one({"User": userName}, {
-                   "$push": {storageLocation: dict(foodItem)}})
+                   "$push": {storageLocation: newItem}})
